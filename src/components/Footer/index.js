2034f8599bc4da41b9a153c0aa367e6737c811c5
Shy <fleetwoodshybreasia@gmail.com>
2022-03-27 19:50:30 -0400

    added more components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <footer className="footer bg-dark fixed-bottom">
+        <a href="https://github.com/Shy-F"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
+        <a href="https://www.linkedin.com/in/shybreasia-fleetwood/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        <a href=""><img src="https://img.icons8.com/ios-glyphs/48/ffffff/trilby.png" alt="" className="icon"/></a>
+    </footer>
+    )
+}
+
+export default Footer;
